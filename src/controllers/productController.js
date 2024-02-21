94fb22dbac8d70385d5e7dfc8f7c97cd31d02236
J8T <jul.milcent@gmail.com>
2024-02-21 16:42:38 +0100

    ✨ Add CRUD operations for products Added controller functions for creating, reading, updating, and deleting products. Each function handles database operations and error handling appropriately.
    - Added getAllProducts, getOneProduct, createProduct, updateProduct, and deleteProduct functions to manage products.
    - Implemented error handling to catch and handle database errors.
    - Updated response messages for better error reporting.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,100 @@
 import Product from "../models/productModel";
 
-export const getAllProducts = async (req, res) => {
+const getAllProducts = async (req, res) => {
   try {
     const allProducts = await Product.find();
     res.send(allProducts);
   } catch (error) {
-    res.send(error);
+    res.status(500).send("Erreur lors de la requête");
   }
+};
+
+const getOneProduct = async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).send("Produit non trouvé");
+    }
+    res.send(product);
+  } catch (error) {
+    res.status(500).send("Erreur lors de la requête");
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      quantityTotal,
+      quantityInStock,
+      location,
+      description,
+      frozen,
+      hasExpirationDate,
+      needBattery,
+    } = req.body;
+
+    const newProduct = new Product({
+      name,
+      quantityTotal,
+      quantityInStock,
+      location,
+      description,
+      frozen,
+      hasExpirationDate,
+      needBattery,
+    });
+
+    await newProduct.save();
+
+    res.status(201).json({ message: "Produit créé", product: newProduct });
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la requête" });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  const updateData = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Produit non trouvé" });
+    }
+
+    res.send(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la requête" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Produit non trouvé" });
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la requête" });
+  }
+};
+
+export {
+  getAllProducts,
+  getOneProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
